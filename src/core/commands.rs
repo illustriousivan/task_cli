c3cfd25dc2140769703f52956c6921af8d1a46c1
Ivan Luiz <m.ivanluiz@gmail.com>
2026-06-20 23:59:34 -0300

    refactor: move Cli to app module and fix import paths

--- src/core/commands.rs
+++ src/core/commands.rs
@@ -1,31 +1,18 @@
-use clap::{Parser, Subcommand};
-
-#[derive(Parser, Debug)]
-pub struct Cli {
-    #[command(subcommand)]
-    pub command: Commands,
-}
+use clap::Subcommand;
 
 #[derive(Subcommand, Debug, Clone, PartialEq)]
 pub enum Commands {
     /// Create a new task with description
     #[command(name = "create")]
-    Create {
-        description: String,
-    },
+    Create { description: String },
 
     /// Remove a task by ID
     #[command(name = "remove")]
-    Remove {
-        id: u32,
-    },
+    Remove { id: u32 },
 
     /// Update an existing task's description
     #[command(name = "update")]
-    Update {
-        id: u32,
-        description: String,
-    },
+    Update { id: u32, description: String },
 
     /// List all tasks
     #[command(name = "list")]
